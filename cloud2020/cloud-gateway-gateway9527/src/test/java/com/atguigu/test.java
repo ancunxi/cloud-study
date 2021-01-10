3d6e93d3890c25529d06b397e70e5994ccc04265
ancunxi <ancunxi666@126.com>
2021-01-10 20:06:48 +0800

    初始化

---
+++ cloud2020/cloud-gateway-gateway9527/src/test/java/com/atguigu/test.java
@@ -0,0 +1,10 @@
+package com.atguigu;
+
+import java.time.ZonedDateTime;
+
+public class test {
+    public static void main(String[] args) {
+        ZonedDateTime abj = ZonedDateTime.now();
+        System.out.println(abj);
+    }
+}
